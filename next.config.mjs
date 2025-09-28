39a7819d65abb08d9f61f73ea97568a5a779fadd
Hassan Mansuri <hassanmansuri570@gmail.com>
2025-09-28 17:38:02 +0530

    Remove framer-motion dependency and reset next.config.mjs to default configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ['framer-motion'],
-  experimental: {
-    esmExternals: false
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
