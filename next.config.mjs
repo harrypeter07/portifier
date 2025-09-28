4ac60113406f118a3702ac4469b725384237c7bd
Hassan Mansuri <hassanmansuri570@gmail.com>
2025-09-28 17:37:33 +0530

    Remove custom webpack alias for framer-motion and add experimental esmExternals setting in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['framer-motion'],
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'framer-motion': 'framer-motion/dist/framer-motion'
-    };
-    return config;
+  experimental: {
+    esmExternals: false
   }
 };
 
