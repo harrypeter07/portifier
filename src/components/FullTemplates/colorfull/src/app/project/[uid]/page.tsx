7d5d6b02575924782696503605a414f9819d7e05
Hassan Mansuri <hassanmansuri570@gmail.com>
2025-10-14 15:59:09 +0530

    dvdfbdf

--- src/components/FullTemplates/colorfull/src/app/project/[uid]/page.tsx
+++ src/components/FullTemplates/colorfull/src/app/project/[uid]/page.tsx
@@ -1,12 +1,12 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
-import CustomSliceZone from "@/components/CustomSliceZone";
-import { getProjectByUID, getProjectsData } from "@/lib/data";
+import CustomSliceZone from "../../../components/CustomSliceZone";
+import { getProjectByUID, getProjectsData } from "../../../lib/data";
 
 type Params = { uid: string };
 
-export default async function Page({ params }: { params: Promise<Params> }) {
-  const { uid } = await params;
+export default async function Page({ params }: { params: Params }) {
+  const { uid } = params;
   const page = await getProjectByUID(uid);
   
   if (!page) {
@@ -19,9 +19,9 @@ export default async function Page({ params }: { params: Promise<Params> }) {
 export async function generateMetadata({
   params,
 }: {
-  params: Promise<Params>;
+  params: Params;
 }): Promise<Metadata> {
-  const { uid } = await params;
+  const { uid } = params;
   const page = await getProjectByUID(uid);
   
   if (!page) {
